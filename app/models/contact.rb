8129f14f1e31e5647e3fdc7ec5a6121c656ebf40
Wagner Almeida <wdmeida@gmail.com>
2017-12-12 19:37:30 -0200

    Customize models in json response

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,19 @@ class Contact < ApplicationRecord
     'Wagner Almeida'
   end
 
+  def kind_description
+    self.kind.description
+  end
+
   def as_json(options = {})
-    super(methods: :author, root: true)
+    super(
+      root: true,
+      methods: [:kind_description, :author],
+      include: {
+        kind: {
+          only: :description
+        }
+      }
+    )
   end
 end
