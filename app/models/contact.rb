8b31b5b00892b65f5ad90a94cd861b00ab0cd53f
Wagner Almeida <wdmeida@gmail.com>
2017-12-13 18:12:21 -0200

    Adding nested attributes for phones in contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,19 +3,27 @@ class Contact < ApplicationRecord
   # Associations 
   belongs_to :kind #, optional: true
   has_many :phones
+  
+  accepts_nested_attributes_for :phones
 
   # def birthdate_br
   #   I18n.l(self.birthdate) unless self.birthdate.blank?
   # end
 
-  def to_br
-    {
-      name: self.name,
-      email: self.email,
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?),
-      kind_id: self.kind_id
-    }
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
   end
+
+  # def to_br
+  #   {
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?),
+  #     kind_id: self.kind_id
+  #   }
+  # end
 
   # def author
   #   'Wagner Almeida'
