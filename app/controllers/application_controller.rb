e6ec2699d417e828a7f4cdd2587c5cd0a2dc36e4
Wagner Almeida <wdmeida@gmail.com>
2017-12-14 11:32:09 -0200

    Improving CRUD to address resource and enable/configure rack-cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+    def record_not_found
+      render json: { error: I18n.t('error.not_found') }, status: :not_content
+    end
 end
