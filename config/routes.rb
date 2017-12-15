f05df9c2258d11cb8125f14834fc3c867e88f14e
Wagner Almeida <wdmeida@gmail.com>
2017-12-15 14:47:56 -0200

    CRUD has_many to Phones

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     resource :kind, only: :show
     resource :kind, only: :show, path: 'relationships/kind'
 
-    resource :phones, only: :show
+    resource :phone, only: [:create, :destroy, :update]
+    resource :phone, only: [:create, :destroy, :update], path: 'relationships/phones'
+
+    resource :phones, only: :show    
     resource :phones, only: :show, path: 'relationships/phones'
   
     resource :address, only: [:create, :destroy, :show, :update]
