bcf95f2a2aa033e1a950b8988daf2c55b4aa6b71
Wagner Almeida <wdmeida@gmail.com>
2017-12-18 09:49:19 -0200

    Added error rendering using the jsonapi.org specification

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,6 +11,10 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
 
+  # Validations
+  validates_presence_of :kind
+  validates_presence_of :address
+
   # def birthdate_br
   #   I18n.l(self.birthdate) unless self.birthdate.blank?
   # end
